7e8d9ea03d01a05064cadf4f2148871a43864410
wjkang <ruoxieme@gmail.com>
2022-07-03 17:56:02 +0800

    remove babel-preset-vca-jsx

--- babel.config.js
+++ babel.config.js
@@ -5,5 +5,5 @@ module.exports = {
       { libraryName: "ant-design-vue", libraryDirectory: "es", style: "css" }
     ] // `style: true` 会加载 less 文件
   ],
-  presets: ["vca-jsx", "@vue/app"]
+  presets: ["@vue/app"]
 };
